5acf3cdc60ed20e27d8d6a0521f00e102317167e
bgtvalex <bgtvalex@ya.ru>
2020-10-12 10:15:44 +0300

    add html

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let start = document.querySelector('#start'),
+
+// Value
+	budgetValue = document.querySelector('.budget-value'),
+	daybudgetValue = document.querySelector('.daybudget-value'),
+	levelValue = document.querySelector('.level-value'),
+	expensesValue = document.querySelector('.expenses-value'),
+	optionalexpensesValue = document.querySelector('.optionalexpenses-value'),
+	incomeValue = document.querySelector('.income-value'),
+	monthsavingsValue = document.querySelector('.monthsavings-value'),
+	yearSavingsValue = document.querySelector('.yearsavings-value'),
+	yearValue = document.querySelector('.year-value'),
+	monthValue = document.querySelector('.month-value'),
+	dayValue = document.querySelector('.day-value'),
+
+// Input
+	
+	expensesItem = document.querySelectorAll('.expenses-item'),
+	optionalexpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+
+// Button
+	expensesItemBtn = document.querySelector('button .expenses-item-btn'),
+	countBudgetBtn = document.querySelector('button .count-budget-btn'),
+
+// Other
+	savings = document.querySelector('#savings')
